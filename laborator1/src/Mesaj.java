c8678be8a76713a1565e35ffd69f63dca11564ec
maguianca <maguiancaelena@gmail.com>
2025-02-28 12:43:49 +0200

    laborator1 tema ora 2

---
+++ laborator1/src/Mesaj.java
@@ -0,0 +1,7 @@
+public class Mesaj {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing i
+        System.out.printf("Buna ziua Salut!");
+    }
+}
